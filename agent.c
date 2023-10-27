d4493d0cf9fae42f88fc69b19e536a243cf2824e
CoderEx24 <coderex24@protonmail.com>
2023-10-27 23:54:59 +0200

    started implementing breadth first

--- agent.c
+++ agent.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <glib.h>
 #include "agent.h"
 #include "raylib.h"
 
@@ -64,4 +65,68 @@ size_t solve_by_depth_first(Grid grid, unsigned short agent_turn)
     free(local_grid);
     return agent_square;
     
+}
+
+size_t solve_by_breadth_first(Grid grid, unsigned short agent_turn)
+{
+    Grid local_grid = grid_copy(grid);
+
+    size_t agent_square = 0;
+    bool solution_found = false;
+
+    for (; agent_square < 9 && !solution_found; agent_square ++)
+    {
+        if (local_grid[agent_square] != EMPTY)
+            continue;
+
+        Grid tree_root = grid_copy(local_grid);
+        GQueue *queue = g_queue_new();
+
+        tree_root[agent_square] = agent_turn;
+        g_queue_push_tail(queue, tree_root);
+
+        while (!g_queue_is_empty(queue))
+        {
+            Grid current_node = g_queue_pop_head(queue);
+
+            if (grid_get_winner(current_node) == agent_turn)
+            {
+                solution_found = true;
+                break;
+            }
+
+            for (size_t enemy_response = 0; enemy_response < 9; enemy_response ++)            
+            {
+                if (current_node[enemy_response] != EMPTY)
+                    continue;
+
+                Grid node_after_enemy_response = grid_copy(current_node);
+                node_after_enemy_response[enemy_response] = OPPOSITE(agent_turn);
+
+                for (size_t future_move = 0; future_move < 9; future_move ++)
+                {
+                    if (node_after_enemy_response[future_move] != EMPTY)
+                        continue;
+
+                    Grid node_of_future_move = grid_copy(node_after_enemy_response);
+                    node_of_future_move[future_move] = agent_turn;
+
+                    g_queue_push_tail(queue, node_of_future_move);
+
+                    TraceLog(LOG_INFO, " | %c | %c | %c | ", STRINGIFY(node_of_future_move[0]), STRINGIFY(node_of_future_move[1]), STRINGIFY(node_of_future_move[2]));
+                    TraceLog(LOG_INFO, " | %c | %c | %c | ", STRINGIFY(node_of_future_move[3]), STRINGIFY(node_of_future_move[4]), STRINGIFY(node_of_future_move[5]));
+                    TraceLog(LOG_INFO, " | %c | %c | %c | \n", STRINGIFY(node_of_future_move[6]), STRINGIFY(node_of_future_move[7]), STRINGIFY(node_of_future_move[8]));
+
+                }
+
+            }
+        }
+
+        free(tree_root);
+        
+        g_queue_free_full(queue, free);
+    }
+    
+    free(local_grid);
+    return agent_square;
 }
